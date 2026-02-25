897a33aabfdfd290b22fbb04607aa722e535dc53
Leah Moshe <moshelea123@gmail.com>
2026-02-25 20:11:32 +0200

    Check for existing collections before creation in migration script

--- migrations/001_create_collections.py
+++ migrations/001_create_collections.py
@@ -3,6 +3,8 @@ from config.mongo_config import get_db
 def run():
     db = get_db()
     # יצירת collections
-    db.create_collection("users")
-    db.create_collection("products")
+    if "users" not in db.list_collection_names():
+        db.create_collection("users")
+    if "products" not in db.list_collection_names():
+        db.create_collection("products")
     print("Collections created: users, products")
